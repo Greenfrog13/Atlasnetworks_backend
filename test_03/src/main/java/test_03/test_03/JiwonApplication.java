9053a8c31228ef79b6b134bf7ffc513db85100c9
JiwonJeong <50576758+Greenfrog13@users.noreply.github.com>
2022-08-03 17:29:48 +0900

    8.3(수) 코드

---
+++ test_03/src/main/java/test_03/test_03/JiwonApplication.java
@@ -0,0 +1,13 @@
+package test_03.test_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiwonApplication.class, args);
+	}
+
+}
---
+++ test_03/src/test/java/test_03/test_03/JiwonApplicationTests.java
@@ -0,0 +1,13 @@
+package test_03.test_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
